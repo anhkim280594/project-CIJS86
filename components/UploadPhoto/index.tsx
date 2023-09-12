b41c050ed1ef2217273e8bd8c6d4675b4e546dca
tuantran <anhkim280594@gmail.com>
2023-09-12 17:44:45 +0700

    update

--- components/UploadPhoto/index.tsx
+++ components/UploadPhoto/index.tsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 export default function SamplePhotosUploader(props): ReactElement {
     const getUploadParams: IDropzoneProps['getUploadParams'] = async ({ file, meta: { name, type, previewUrl } }) => {
-        const data = await axios.post(`https://api-gateway.prod.rootopia.vn/forms/presigned-url`, {
+        const data = await axios.post(`https://api-gateway.stg.rootopia.vn/forms/presigned-url`, {
             filename: name,
             filetype: type,
         }
